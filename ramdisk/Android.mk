a927f6da6ccdf374563935466c367b6f2e6fd048
Samuel Caldas <samuel.caldas@gmail.com>
2020-10-25 02:28:12 -0300

    Correcting misspelled filename.
    Change-Id: Iff2ba81659c8e7772bda0ccdce25e849295fbe28

--- ramdisk/Android.mk
+++ ramdisk/Android.mk
@@ -28,7 +28,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE		:= universal5420.wifi.rc
 LOCAL_MODULE_TAGS	:= optional eng
 LOCAL_MODULE_CLASS	:= ETC
-LOCAL_SRC_FILES		:= universal5422.wifi.rc
+LOCAL_SRC_FILES		:= init.universal5422.wifi.rc
 LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
